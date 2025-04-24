45240d95e06be6b3aba91da04e606c534aa9e333
sbchild <sbchild0@gmail.com>
2025-04-25 00:20:21 +0800

    commit
    Signed-off-by: sbchild <zinu5k7d1k2uzsxa@proton.me>

---
+++ web/src/components/TopBar/index.tsx
@@ -0,0 +1,108 @@
+import {
+  Badge,
+  Breadcrumbs,
+  Button,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField,
+  Tooltip,
+} from "@mui/material";
+import * as styles from "./index.module.css";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import InfoIcon from "@mui/icons-material/Info";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { useSignal, useComputed } from "@preact/signals";
+
+const SearchBox = () => {
+  const searchText = useSignal("");
+  const searchTextEmpty = useComputed(() => searchText.value.length <= 0);
+  const searchFocus = useSignal(false);
+  const searchShowLabel = useComputed(
+    () => searchTextEmpty.value && !searchFocus.value,
+  );
+  const searchLabelText = "色妹妹的船新博客";
+  const searchLabelText2 = "键入以搜索";
+  const searchFocused = () => {
+    searchFocus.value = true;
+  };
+  const searchBlured = () => {
+    searchFocus.value = false;
+  };
+
+  return (
+    <TextField
+      // hiddenLabel={!searchShowLabel.value}
+      onFocus={searchFocused}
+      onBlur={searchBlured}
+      defaultValue={""}
+      value={searchText}
+      onChange={(ev) => {
+        // @ts-ignore
+        searchText.value = ev.target?.value;
+      }}
+      label={searchShowLabel.value ? searchLabelText : searchLabelText2}
+      variant="filled"
+      size="small"
+      slotProps={{
+        input: {
+          startAdornment: searchShowLabel.value ? null : (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.placeholder} />
+      <div className={styles.body}>
+        <div className={styles.body_logo}>logo</div>
+        <div className={styles.body_search}>
+          <SearchBox />
+        </div>
+        <div className={styles.body_title}>
+          <Breadcrumbs
+            separator={<NavigateNextIcon fontSize="small" />}
+            aria-label="breadcrumb"
+          >
+            <div>首页</div>
+            <div>友情链接</div>
+          </Breadcrumbs>
+        </div>
+        <div className={styles.body_links}>
+          <Stack direction="row" spacing={1}>
+            <Tooltip title="友情链接">
+              <IconButton color="secondary">
+                <FavoriteIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="关于博主">
+              <IconButton color="primary">
+                <InfoIcon />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+        </div>
+        <div className={styles.body_right}>
+          <Tooltip title="查看通知">
+            <IconButton>
+              <Badge badgeContent={2} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,51 +1,67 @@
 import { render } from "preact";
-import App from "./App";
 import {
-	LocationProvider,
-	ErrorBoundary,
-	Route,
-	Router,
-	lazy,
+  ErrorBoundary,
+  LocationProvider,
+  Route,
+  Router,
+  lazy,
 } from "preact-iso";
-import NotFound from "./NotFound";
-import Layout from "./Layout";
+import App from "./App";
 import Background from "./Background";
+import Layout from "./Layout";
+import NotFound from "./NotFound";
 import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const LazyIndex = lazy(async () => App);
 const LazyNotFound = lazy(async () => NotFound);
 
 const SiteRouter = () => {
-	return (
-		<div className="router">
-			<LocationProvider>
-				<ErrorBoundary>
-					<Router>
-						<Route path="/" component={LazyIndex} />
+  return (
+    <div className="router">
+      <LocationProvider>
+        <ErrorBoundary>
+          <Router>
+            <Route path="/" component={LazyIndex} />
 
-						{/* <Route path="/profiles" component={Profiles} />
+            {/* <Route path="/profiles" component={Profiles} />
 						<Route path="/profile/:id" component={Profile} /> */}
 
-						<Route component={LazyNotFound} default />
-					</Router>
-				</ErrorBoundary>
-			</LocationProvider>
-		</div>
-	);
+            <Route component={LazyNotFound} default />
+          </Router>
+        </ErrorBoundary>
+      </LocationProvider>
+    </div>
+  );
 };
 
 const SiteRoot = () => {
-	return (
-		<div className="site-root">
-			<Background />
-			<Layout>
-				<SiteRouter />
-			</Layout>
-		</div>
-	);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="site-root">
+        <Background />
+        <Layout>
+          <SiteRouter />
+        </Layout>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 const root = document.getElementById("root");
 if (root) {
-	render(<SiteRoot />, root);
+  render(<SiteRoot />, root);
 }
